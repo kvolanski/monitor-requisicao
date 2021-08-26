68420ee0afdf74831fafd772cdad911cadfb4d25
Kevin <kvolanski@gmail.com>
2021-08-26 10:43:44 -0300

    Método delete no RequisicaoController

--- src/main/java/com/kvolanski/mr/controller/RequisicaoController.java
+++ src/main/java/com/kvolanski/mr/controller/RequisicaoController.java
@@ -1,13 +1,17 @@
 package com.kvolanski.mr.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +42,17 @@ public class RequisicaoController {
 	public Optional<RequisicaoModel> find(@PathVariable String nome) {
 		return repository.findByNome(nome);
 	}
+		
+	@DeleteMapping(path ={"/{nome}"})
+	public ResponseEntity <?> delete(@PathVariable String nome) {		
+	   return repository.findById(repository.findByNome(nome).get().getId())
+	           .map(record -> {
+	               repository.deleteById(repository.findByNome(nome).get().getId());
+	               return ResponseEntity.ok().build();
+	           }).orElse(ResponseEntity.notFound().build());
+	}
+	
+
 
 
 }
